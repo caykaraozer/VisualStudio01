a2414895f0277f411af65e9c5b71356e46aaafd1
caykaraozer <ozer.caykara@bts-tr.com>
2018-08-02 23:45:27 +0300

    yes

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Repository
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Merhaba Dünya");
         }
     }
 }
